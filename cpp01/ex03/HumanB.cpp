608918b606b00125952261de192755dead271bde
sknefi <filip.karika1@gmail.com>
2025-07-20 10:49:04 +0200

    01 03 finished

---
+++ cpp01/ex03/HumanB.cpp
@@ -0,0 +1,22 @@
+#include "HumanB.hpp"
+
+HumanB::HumanB( std::string name ):
+	_name(name),
+	_weapon(nullptr)
+{
+};
+
+HumanB::~HumanB() { };
+
+void	HumanB::attack( void ) const
+{
+	if (_weapon)
+		std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
+	else
+		std::cout << _name << " has no weapon to attack with" << std::endl;
+}
+
+void	HumanB::setWeapon( Weapon &weapon )
+{
+	_weapon = &weapon;
+}
