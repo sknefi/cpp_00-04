06916c6bc298d061551131f25413e79a62eaab51
sknefi <filip.karika1@gmail.com>
2025-08-15 15:01:37 +0200

    03 00 finished

---
+++ cpp03/ex00/main.cpp
@@ -0,0 +1,20 @@
+#include "ClapTrap.hpp"
+
+int		main()
+{
+	ClapTrap a("Alpha");
+	ClapTrap b("Beta");
+
+	a.attack("Beta");
+	b.takeDamage(0);
+
+	b.beRepaired(5);
+
+	for (int i = 0; i < 11; ++i)
+		a.attack("Dummy");
+
+	b.takeDamage(20);
+	b.beRepaired(3);
+
+	return (EXIT_SUCCESS);
+}
