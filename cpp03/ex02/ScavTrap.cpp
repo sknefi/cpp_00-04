959d66bf77f2be961a8fb2f948f22df65306b693
sknefi <filip.karika1@gmail.com>
2025-08-17 15:02:01 +0200

    03 01 finished

---
+++ cpp03/ex02/ScavTrap.cpp
@@ -0,0 +1,57 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap() : ClapTrap()
+{
+	// name is set in ClapTrap constructor
+	_hp = 100;
+	_energy = 50;
+	_ad = 20;
+	std::cout << "ScavTrap " << _name << " default constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap( const std::string &name ) : ClapTrap( name )
+{
+	// name is set in ClapTrap constructor
+	_hp = 100;
+	_energy = 50;
+	_ad = 20;
+	std::cout << "ScavTrap " << _name << " parametric constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap( const ScavTrap &other ) : ClapTrap( other )
+{
+	ClapTrap::operator=( other );
+	_hp = other._hp;
+	_energy = other._energy;
+	_ad = other._ad;
+	std::cout << "ScavTrap " << _name << " copy constructor called" << std::endl;
+}
+
+ScavTrap &ScavTrap::operator=( const ScavTrap &other )
+{
+	ClapTrap::operator=( other );
+	_hp = other._hp;
+	_energy = other._energy;
+	_ad = other._ad;
+
+	std::cout << "ScavTrap " << _name << " copy assignment operator called" << std::endl;
+	return (*this);
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap " << _name << " destructor called" << std::endl;
+}
+
+void	ScavTrap::attack( const std::string &target )
+{
+	if (!validStats())
+		return ;
+	std::cout << "ScavTrap " << _name << " attacks " << target << ", causing " << _ad << " points of damage!" << std::endl;
+	_energy--;
+}
+
+void	ScavTrap::guardGate()
+{
+	std::cout << "ScavTrap " << _name << " is now in Gate keeper mode." << std::endl;
+}
---
+++ cpp03/ex02/main.cpp
@@ -0,0 +1,26 @@
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int		main()
+{
+	// test ScavTrap
+	ScavTrap a("Alpha");
+	ScavTrap b("Beta");
+
+	a.attack("Beta");
+	b.takeDamage(20);
+
+	b.beRepaired(5);
+
+	for (int i = 0; i < 11; ++i)
+		a.attack("Dummy");
+
+	for (int i = 0; i < 5; ++i) // Beta should die here
+		b.takeDamage(20);
+
+	b.beRepaired(3);
+
+	a.guardGate();
+
+	return (EXIT_SUCCESS);
+}
